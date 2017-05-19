f232772cbd0f15ffc340fd120526216488bb3956
Peter Heiss <peter.heiss91@gmail.com>
2017-05-20 00:39:46 +0200

    Asteroids and first logic

---
+++ Assets/AsteroidSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour {
+    public int maxCountAsteroids;
+
+    public GameObject player;
+
+    private Vector3 middlePoint;
+
+    public List<GameObject> asteroidAssets;
+    private List<GameObject> asteroids = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        middlePoint = player.transform.position;
+        for(int i = 0; i < maxCountAsteroids; i++){
+            spawnAsteroids();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void spawnAsteroids()
+    {
+        /* Hier können verschiedene Größen genommen werden*/
+        int rand = Random.Range(0, asteroidAssets.Count);
+        Debug.Log(rand);
+        GameObject randomGO = asteroidAssets[rand];
+
+        // Spawn that shit
+        GameObject go = Instantiate(randomGO, player.transform.position, Quaternion.identity);
+        asteroids.Add(go);
+    }
+}
---
+++ Assets/AstroidDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidDestroy : MonoBehaviour {
+    public GameObject childGO;
+    public int countChilds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Falls ein Asteroid zerstört wird, so soll er Kinder spawnen lassen
+    private void OnDestroy()
+    {
+        spawnChildAsteroids();
+    }
+
+    // Spawnt die Kinder-Asteroiden
+    void spawnChildAsteroids()
+    {
+        for (int i = 0; i < countChilds; i++)
+        {
+            Instantiate(childGO, transform.position, Quaternion.identity);
+        }
+    }
+}
