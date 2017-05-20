32b9f4a9da86092521ce976bebbd3d4504cd747c
p_kath01 <p_kath01@uni-muenster.de>
2017-05-20 20:08:19 +0200

    PlasmaBomb Prefab

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour {
+
+    private float initTime;
+
+	// Use this for initialization
+	void Start () {
+        initTime = Time.fixedTime;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+        if(Time.fixedTime - initTime > 2.2)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+
+    void OnTriggerStay(Collider collision)
+    {
+        if ((Time.fixedTime - initTime > 2))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
